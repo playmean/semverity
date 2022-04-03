f6dc7ec7667afde89e0d5d2b160f294952e60332
Aleksey Korolev <im.playmean@gmail.com>
2022-04-03 21:51:06 +0300

    fix: add interpreter path to bin file

--- tools/release.sh
+++ tools/release.sh
@@ -4,6 +4,8 @@ VERSION=$(node dist/index.min.js bump)
 
 node dist/index.min.js patch --files package.json package-lock.json tools/copyright.txt
 
+sed -i '1s/^/#!\/usr\/bin\/env node\n/' dist/index.min.js
+
 ./tools/copyright.sh
 
 git add .
