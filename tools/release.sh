49bb92a86ee3d35bc7d67ce26024b99ca4a07c10
Aleksey Korolev <im.playmean@gmail.com>
2022-04-03 20:24:11 +0300

    chore: release script commits copyright too

--- tools/release.sh
+++ tools/release.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
-node dist/index.min.js patch --files package.json package-lock.json tools/copyright.txt --commit bump
+VERSION=$(node dist/index.min.js bump)
+
+node dist/index.min.js patch --files package.json package-lock.json tools/copyright.txt
 
 ./tools/copyright.sh
+
+git add .
+git commit -m "bump: $VERSION"
